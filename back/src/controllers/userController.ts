38c8e9b6c28686bc59847028dc4d93511451d4e9
한동진 <arkc1009@gmail.com>
2021-12-17 11:01:38 +0900

    Back/feat: 유저 프로필 create, update 구현

---
+++ back/src/controllers/userController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { UserModifyType } from '../interfaces/User';
+import { findUser, modifyUser } from '../service/userService';
+import { makeError } from '../utils/makeError';
+
+export const getProfile = (req: Request, res: Response) => {
+  const user = findUser(res.locals.email);
+  if (!user) {
+    makeError(res, 407, '유저를 찾을 수 없습니다.');
+    return;
+  }
+
+  const { email, name, location } = user;
+  res.status(200).json({
+    isSuccess: true,
+    msg: '성공적으로 프로필을 가져왔습니다.',
+    data: { email, name, location },
+  });
+};
+
+export const updateProfile = (req: Request, res: Response) => {
+  const data: UserModifyType = req.body;
+
+  const user = modifyUser(res.locals.email, data);
+  if (!user) {
+    makeError(res, 407, '프로필 수정을 실패했습니다.');
+    return;
+  }
+
+  const { email, name, location } = user;
+  res.status(200).json({
+    isSuccess: true,
+    msg: '성공적으로 프로필을 수정했습니다.',
+    data: { email, name, location },
+  });
+};
